3c4c847e8432e8e0422e13cee5efc92ffabbfaae
mitdonga5344 <mitdonga5344@gmail.com>
2023-06-23 21:59:08 +0530

    Fixing migrations

--- db/migrate/20220917143203_change_in_columns_in_order_items.rb
+++
@@ -1,9 +0,0 @@
-class ChangeInColumnsInOrderItems < ActiveRecord::Migration[7.0]
-  def up
-		add_column :order_items, :order_id, :integer, null: true
-  end
-  def down
-		remove_column :order_items, :order_id
-		# remove_foreign_key :order_items, :orders
-  end
-end
